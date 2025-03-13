5bba99d99273c0c92dca5f0d0d93d6dffcb82e21
Felipe Santiago Morais <fillikz@gmail.com>
2025-03-13 13:17:19 -0300

    refactor(Header): refactor component to include Logo and NavBar components

---
+++ src/app/components/header/logo/index.tsx
@@ -0,0 +1,12 @@
+import { NotebookPen } from "lucide-react";
+
+export const Logo = () => (
+  <section className="flex items-center gap-2">
+    <div className="bg-primary ring-primary-foreground rounded-full p-2 ring-2">
+      <NotebookPen className="size-6" />
+    </div>
+    <h1 className="after:from-primary-foreground after:via-primary relative to-transparent text-2xl font-semibold after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:rounded-full after:bg-linear-to-r">
+      Freelancer Registration
+    </h1>
+  </section>
+);
