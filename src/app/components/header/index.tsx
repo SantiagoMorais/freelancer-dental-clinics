087a3be723fe9be08078da0d381d87563f78fcbc
Felipe Santiago Morais <fillikz@gmail.com>
2025-03-13 13:31:18 -0300

    feat(ThemeTogglerButton): add component for theme switching

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,9 +1,11 @@
 import { Logo } from "./logo";
 import { NavBar } from "./nav-bar";
+import { ThemeTogglerButton } from "./theme-toggler-button";
 
 export const Header = () => (
   <header className="flex w-full max-w-(--breakpoint-2xl) items-center justify-between">
     <Logo />
     <NavBar />
+    <ThemeTogglerButton />
   </header>
 );
