88e57ec308c6cf43d24aadfc1c5deb51d031ad7d
Felipe Santiago Morais <fillikz@gmail.com>
2025-03-13 13:32:24 -0300

    style(Logo): update class names gradient to improve style

--- src/app/components/header/logo/index.tsx
+++ src/app/components/header/logo/index.tsx
@@ -5,7 +5,7 @@ export const Logo = () => (
     <div className="bg-primary ring-primary-foreground rounded-full p-2 ring-2">
       <NotebookPen className="size-6" />
     </div>
-    <h1 className="after:from-primary-foreground after:via-primary relative to-transparent text-2xl font-semibold after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:rounded-full after:bg-linear-to-r">
+    <h1 className="after:from-primary after:via-primary relative to-transparent text-2xl font-semibold after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:rounded-full after:bg-linear-to-r">
       Freelancer Registration
     </h1>
   </section>
