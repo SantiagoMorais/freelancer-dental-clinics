b495d7444ccac602972a86b79e7b57897ae8e5de
Felipe Santiago Morais <fillikz@gmail.com>
2025-03-18 11:36:56 -0300

    fix: update package-lock.json and clean up code formatting in multiple files

--- src/functions/get-address-by-zip-code.ts
+++ src/functions/get-address-by-zip-code.ts
@@ -13,7 +13,9 @@ export const getAddressByZipCode = async (
   const formatedZipCode = zipCode.replace(/-/g, "");
 
   try {
-    const response = await fetch(`https://viacep.com.br/ws/${formatedZipCode}/json/`);
+    const response = await fetch(
+      `https://viacep.com.br/ws/${formatedZipCode}/json/`
+    );
 
     if (!response.ok) throw new Error("Falha ao procurar endereço.");
 
