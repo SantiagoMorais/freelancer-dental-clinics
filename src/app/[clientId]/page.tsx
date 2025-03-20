90bcebde3effc1836f044c6317fa1c680a75e2d2
Felipe Santiago Morais <fillikz@gmail.com>
2025-03-20 19:14:00 -0300

    feat(ClientDetails): add header component to client details page

---
+++ src/app/[clientId]/page.tsx
@@ -0,0 +1,11 @@
+import { Header } from "./components/header";
+
+const ClientDetailsPage = () => {
+  return (
+    <section className="flex min-h-screen flex-col items-center gap-8 p-4 sm:px-8">
+      <Header />
+    </section>
+  );
+};
+
+export default ClientDetailsPage;
