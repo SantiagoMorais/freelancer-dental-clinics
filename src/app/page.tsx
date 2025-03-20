2ad567638519beb1a2b86a5d2f09e0bf01be3212
Felipe Santiago Morais <fillikz@gmail.com>
2025-03-20 16:23:28 -0300

    style(Home): increase gap between header and main components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Main } from "./components/main";
 
 export default function Home() {
   return (
-    <section className="flex min-h-screen flex-col items-center p-4 sm:px-8">
+    <section className="flex min-h-screen flex-col items-center gap-8 p-4 sm:px-8">
       <Header />
       <Main />
     </section>
