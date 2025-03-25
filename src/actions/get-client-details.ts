bdd2c925c7386ec60ae11989d4aff9d198f4a190
Felipe Santiago Morais <fillikz@gmail.com>
2025-03-25 09:33:44 -0300

    chore(getClientDetails): remove clientProjects from client details retrieval

--- src/actions/get-client-details.ts
+++ src/actions/get-client-details.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { Client, ClientProject } from "@prisma/client";
+import { Client } from "@prisma/client";
 
 import { db } from "@/lib/prisma";
 
@@ -8,12 +8,11 @@ export const getClientDetails = async ({
   clientId,
 }: {
   clientId: string;
-}): Promise<Client & { clientProjects: ClientProject[] }> => {
+}): Promise<Client> => {
   const client = await db.client.findUnique({
     where: {
       id: clientId,
     },
-    include: { clientProjects: true },
   });
 
   if (!client) throw new Error("The id doesn't match with any client register");
