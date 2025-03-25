53e942a756153c2a1fef78b997bab2679fb8102a
Felipe Santiago Morais <fillikz@gmail.com>
2025-03-25 13:43:32 -0300

    refactor(updateProject): remove outdated project update logic and associated types

--- src/core/types/update-concluded-project-schema.ts
+++
@@ -1,5 +0,0 @@
-import { TConcludeProjectSchema } from "./conclude-project-schema";
-import { TRegisterProjectSchema } from "./register-project-schema";
-
-export type TUpdateConcludedProjectSchema = TConcludeProjectSchema &
-  TRegisterProjectSchema;
