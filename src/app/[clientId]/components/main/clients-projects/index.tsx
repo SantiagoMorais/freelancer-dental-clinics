1733fb7fa348ae85f977a6a80b1b4fc94a7e2609
Felipe Santiago Morais <fillikz@gmail.com>
2025-03-26 18:06:42 -0300

    fix(ClientsProjects): adjust minimum height for responsive layout

--- src/app/[clientId]/components/main/clients-projects/index.tsx
+++ src/app/[clientId]/components/main/clients-projects/index.tsx
@@ -4,7 +4,7 @@ import { ProjectsList } from "./projects-list";
 import { RegisterClientProjectsSheet } from "./register-client-projects-sheet";
 
 export const ClientsProjects = () => (
-  <section className="bg-muted/50 border-muted flex w-full flex-1 flex-col items-center gap-4 overflow-hidden rounded-lg border p-4 md:flex-1 md:p-6">
+  <section className="bg-muted/50 border-muted flex min-h-100 w-full flex-1 flex-col items-center gap-4 overflow-hidden rounded-lg border p-4 md:min-h-auto md:flex-1 md:p-6">
     <h2 className="bg-secondary/50 w-fit truncate rounded-md px-2 py-1 pt-0 text-center text-xl font-semibold">
       Projetos do cliente
     </h2>
